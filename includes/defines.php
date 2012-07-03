269175d57d01a67ddc9e15719145a166ab973bca
florianv <voutzinos.florian@gmail.com>
2012-07-03 09:02:02 +0200

    Initial commit

---
+++ includes/defines.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package    Petrinet
+ * @copyright  Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Define the path for the Joomla Platform.
+if (!defined('JPATH_PLATFORM'))
+{
+	$platform = getenv('JPLATFORM_HOME');
+
+	if ($platform)
+	{
+		define('JPATH_PLATFORM', realpath($platform));
+	}
+
+	else
+	{
+		define('JPATH_PLATFORM', realpath(__DIR__ . '/../platform/libraries'));
+	}
+}
+
+define('JPATH_SITE', realpath(__DIR__ . '/../src'));
+define('JPATH_BASE', realpath(__DIR__ . '/../www'));
+define('JPATH_ROOT', realpath(__DIR__ . '/..'));
+//define('JPATH_CONFIGURATION', JPATH_ROOT . '/config');
+//define('JPATH_DB', JPATH_ROOT . '/database');
+//define('JPATH_CACHE', JPATH_ROOT . '/cache');
