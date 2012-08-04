4f73233ca43dcbe2e2e6f6e34eb4e928e88f1b6d
florianv <voutzinos.florian@gmail.com>
2012-08-04 17:21:12 +0200

    begin color implementation

---
+++ src/token/set.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * @package     Petrinet
+ * @subpackage  Token
+ *
+ * @copyright   Copyright (C) 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * Class representing a multiset of tokens.
+ *
+ * @package     Petrinet
+ * @subpackage  Token
+ * @since       1.0
+ */
+class PNTokenSet implements Countable, IteratorAggregate
+{
+	/**
+	 * @var    array  The tokens in this set.
+	 * @since  1.0
+	 */
+	protected $tokens;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $tokens  An array of tokens.
+	 *
+	 * @since   1.0
+	 */
+	public function __construct(array $tokens = array())
+	{
+		empty($tokens) ? $this->tokens = array() : $this->setTokens($tokens);
+	}
+
+	/**
+	 * Check if a token belongs to this set.
+	 *
+	 * @param   PNToken  $token  The token.
+	 *
+	 * @return  boolean  True or false.
+	 *
+	 * @since   1.0
+	 */
+	public function exists(PNToken $token)
+	{
+		$tokenSignature = serialize($token);
+
+		return isset($this->tokens[$tokenSignature]);
+	}
+
+	/**
+	 * Add a token to this set.
+	 *
+	 * @param   PNToken  $token  The token.
+	 *
+	 * @return  PNTokenSet  This method is chainable.
+	 *
+	 * @since   1.0
+	 */
+	public function addToken(PNToken $token)
+	{
+		$tokenSignature = serialize($token);
+
+		if (isset($this->tokens[$tokenSignature]))
+		{
+			$this->tokens[$tokenSignature][] = $token;
+		}
+
+		else
+		{
+			$this->tokens[$tokenSignature] = array($token);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Remove a token from this set.
+	 * Only a copy of one token is removed if it appears more than
+	 * one time in the set.
+	 *
+	 * @param   PNToken  $token  The token.
+	 *
+	 * @return  PNElementSet  This method is chainable.
+	 *
+	 * @since   1.0
+	 */
+	public function removeToken(PNToken $token)
+	{
+		$tokenSignature = serialize($token);
+
+		if (isset($this->tokens[$tokenSignature]))
+		{
+			// If there is only one element.
+			if (count($this->tokens[$tokenSignature]) == 1)
+			{
+				// Delete the element.
+				unset($this->tokens[$tokenSignature]);
+			}
+
+			else
+			{
+				// Just delete one copy of it.
+				array_shift($this->tokens[$tokenSignature]);
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Set the tokens in this set.
+	 *
+	 * @param   array  $tokens  An array of tokens.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function setTokens(array $tokens)
+	{
+		foreach ($tokens as $token)
+		{
+			$this->addToken($token);
+		}
+	}
+
+	/**
+	 * Get the tokens in this set.
+	 *
+	 * @return  array  An array of tokens.
+	 *
+	 * @since   1.0
+	 */
+	public function getTokens()
+	{
+		return $this->tokens;
+	}
+
+	/**
+	 * Get an iterator on the token set.
+	 *
+	 * @return  Traversable  The Iterator.
+	 *
+	 * @since   1.0
+	 */
+	public function getIterator()
+	{
+		return new ArrayIterator($this->tokens);
+	}
+
+	/**
+	 * Clear the set of all its tokens.
+	 *
+	 * @return  array  An array of removed tokens.
+	 *
+	 * @since   1.0
+	 */
+	public function clear()
+	{
+		$tokens = $this->tokens;
+
+		$this->tokens = array();
+
+		return $tokens;
+	}
+
+	/**
+	 * Get the set size.
+	 *
+	 * @return  integer  The number of tokens.
+	 *
+	 * @since   1.0
+	 */
+	public function count()
+	{
+		return count($this->tokens);
+	}
+}
---
+++ src/token/token.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @package     Petrinet
+ * @subpackage  Token
+ *
+ * @copyright   Copyright (C) 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * Base Class for Petri Net (Colored) Tokens.
+ *
+ * @package     Petrinet
+ * @subpackage  Token
+ * @since       1.0
+ */
+class PNToken implements Serializable
+{
+	/**
+	 * @var    PNColor  The token color.
+	 * @since  1.0
+	 */
+	protected $color;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   PNColor  $color  The token color.
+	 *
+	 * @since   1.0
+	 */
+	public function __construct(PNColor $color = null)
+	{
+		$this->color = $color;
+	}
+
+	/**
+	 * Check if the token is colored.
+	 *
+	 * @return  boolean  True if it's the case, false otherwise.
+	 *
+	 * @since   1.0
+	 */
+	public function isColored()
+	{
+		return !is_null($this->color);
+	}
+
+	/**
+	 * Get the Token color.
+	 *
+	 * @return  PNColor  The token color.
+	 *
+	 * @since   1.0
+	 */
+	public function getColor()
+	{
+		return $this->color;
+	}
+
+	/**
+	 * Set the Token color.
+	 *
+	 * @param   PNColor  $color  The token color.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function setColor(PNColor $color)
+	{
+		$this->color = $color;
+	}
+
+	/**
+	 * Serialize the token.
+	 *
+	 * @return  string  The serialized token.
+	 *
+	 * @since   1.0
+	 */
+	public function serialize()
+	{
+		return serialize($this->color);
+	}
+
+	/**
+	 * Unserialize the token.
+	 *
+	 * @param   string  $token  The serialized token.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function unserialize($token)
+	{
+		$this->color = unserialize($token);
+	}
+}
